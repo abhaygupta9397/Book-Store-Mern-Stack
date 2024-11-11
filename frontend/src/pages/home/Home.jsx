fb49dacfed4ca22441cb9279dc50b41473614a77
Abhay Gupta <abhaygupta9397@gmail.com>
2024-11-11 15:43:44 +0530

    my first commit
    done the header portion

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner"
+import TopSellers from './TopSellers';
+
+const home = () => {
+  return (
+    <>
+      <Banner/>
+      <TopSellers/>
+    </>
+  )
+}
+
+export default home
---
+++ frontend/src/pages/home/TopSellers.jsx
@@ -0,0 +1,12 @@
+
+import Banner from './Banner'
+
+const TopSellers = () => {
+  return (
+    <>
+     <Banner/>
+    </>
+  )
+}
+
+export default TopSellers
