cd3aaa967ae0a6834ab676569a801a45d309103e
Abhay Gupta <abhaygupta9397@gmail.com>
2024-12-10 18:38:24 +0530

    Footer Section done
    Footer Section is done on 10 / 12 / 2024

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   const handleLogOut = () => {
     
 }
-  const currentuser = true;
+  const currentuser = false;
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
